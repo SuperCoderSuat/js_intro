0201a792779424d6901710d6e70e70c066177dc3
SuperCoderSuat <supercodesuat@gmail.com>
2023-10-16 14:58:47 +0200

    first commit

---
+++ javascript_intro/js_intro_level_1_3/assets/js/main.js
@@ -0,0 +1,245 @@
+//#################
+// Intro-Level-1_3
+//#################
+console.log("%c Intro-Level-1_3", "color: green");
+
+// let i = 1;
+// console.log(typeof i);
+// let lastName = "Johnson";
+// console.log(typeof lastName);
+// let j = "j";
+// console.log(typeof j);
+// let status = true;
+// console.log(typeof status);
+// let hello = "hello";
+// console.log(typeof hello);
+// let helloWorld = "hello";
+// console.log(typeof helloWorld);
+
+
+//#################
+// Intro-Level-1_4
+//#################
+console.log("%c Intro-Level-1_4", "color: green");
+
+// let name = "John";
+// console.log(typeof name);
+
+// const PI = 3.14;
+// console.log(typeof PI);
+
+// let nan = NaN;
+// console.log(typeof nan);
+
+// let boolean = false;
+// console.log(typeof boolean);
+
+// let list = [1,2,3,4];
+// console.log(typeof list);
+
+// let obj = {name:'John', age:34};
+// console.log(typeof obj);
+
+// let date = new Date();
+// console.log(typeof date);
+
+// let method = function () {};
+// console.log(typeof method);
+
+// let x = null;
+// console.log(typeof x);
+
+// let add = 3+2 == 5;
+// console.log(typeof add);
+
+// let mix = 3 + "3";
+// console.log(typeof mix);
+
+// Intro-Level-1_7
+
+// let helloWorld = "Hello World";
+// console.log(typeof helloworld);
+
+// const pi = 3.14;
+// console.log(typeof pi);
+
+// const burjdubai = "828m";
+// console.log(typeof burjdubai);
+
+// const fullName = "Jan Schmidt";
+// console.log(typeof fullname);
+
+// const eifelTower = "324m";
+// console.log(typeof eifeltower);
+
+// let camelCase = "camelCase";
+// console.log(typeof camelcase);
+
+// let kursStatus = true;
+// console.log(typeof kursstatus);
+
+
+//#####################
+// Variablen-Level-1_5 
+//#####################
+console.log("%c Variablen-Level-1_5", "color: green");
+
+// let carName = "BMW";
+// console.log(carName);
+
+// let x = 150;
+// console.log(x);
+
+// let y = 50;
+// console.log(y);
+
+// let z = x + y;
+// console.log(z);
+
+// let firstName = "John";
+
+// let lastName = "Doe";
+
+// let age = 35;
+
+// console.log("Hi! I am " + firstName + " " + lastName + ", im " + age + " years old and you?")
+
+//#####################
+// Variablen-Level-1_6 
+//#####################
+console.log("%c Variablen-Level-1_6", "color: green");
+
+// let x = 34;
+// console.log(x);
+
+// x = 67;
+// console.log(x);
+
+// const y = 34;
+// y = 67;
+// console.log(y);
+
+
+//#####################
+// Intro-Level-1_8
+//#####################
+console.log("%c Intro-Level-1_8", "color: green");
+
+// let x = 20;
+// let y = 30;
+
+// console.log(x + y);
+// console.log(y - x);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+
+// let z = 10;
+// let resultOne = x * y / z;
+
+// let a = 15;
+// let b = 9;
+// console.log(a % b);
+
+// let c = 20;
+
+// let resultTwo = a + b * c;
+// console.log(resultTwo);
+
+// a = a + 1; a += 1;
+// a++;
+// console.log(a);
+
+// b = b - 1; b -= 1;
+// b--;
+// console.log(b);
+
+// let resultThree = b - a;
+
+// console.log(resultOne % resultTwo);
+
+
+//#####################
+// Intro-Level-1_8
+//#####################
+console.log("%c Intro-Level-1_8", "color: green");
+
+// let x = 55;
+// console.log('result: ' + x);
+
+// x *= 2;
+// console.log('result: ' + x);
+
+// let y = 10;
+// console.log('score: ' + y);
+
+// y *= 2;
+// console.log('score: ' + y);
+
+// for (let i=0; i<4; i++) {
+//     console.log("increment: " + i);
+// }
+
+
+//#####################
+// Intro-Level-1_13
+//#####################
+console.log("%c Intro-Level-1_13", "color: green");
+
+// // Addition
+// let addition_operator = 1 + 1;
+// console.log("addition: " + addition_operator);
+//  // Subtraktion
+// let subtraktion_operator = 2 - 1;
+// console.log("subtraktion: " + subtraktion_operator);
+// // Multiplikation
+// let multiplication_operator = 2 * 2;
+// console.log("multiplication: " + multiplication_operator);
+// // Division
+// let division_operator = 4 / 2;
+// console.log("division: " + division_operator);
+// // Modul: zeigt an, was der Rest sein würde.
+// let modulus_operator = 14 % 5;
+// console.log("modulus: " + modulus_operator);
+
+
+//##########################
+// String-Methods-Level-1_11
+//##########################
+console.log("%c String-Methods-Level-1_11", "color: green");
+
+// let x = "Hello ";
+// let y = "World";
+// let z = x + y;
+// document.write(z + "<br>");
+// console.log(z);
+
+// let n = x + " " + y;
+// document.write(n + "<br>");
+// console.log(n);
+
+// document.write(x + y)
+// console.log(x + y);
+
+// let zweiter = " Ich komme auf Platz zwei";
+// let meinString = "Ich bin Erste:r" + zweiter;
+// console.log(meinString);
+
+
+//##########################
+// Output-Level-1_4
+//##########################
+console.log("%c Output-Level-1_4", "color: green");
+
+// window.alert("Hallo Welt");
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+// let alter = window.prompt( "Gib mir bitte dein Alter:" );
+// console.log(alter);
+// let b = 5;
+// let a = b * 5 -3;
+// window.alert(a);
+// let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+// let grosse = "2,72m";
+// window.confirm(mann + grosse);
